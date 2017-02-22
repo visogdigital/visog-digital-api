31e9ce7da564fb4b8a03f912c768f7e576b02751
SH computeres <SH computeres@SHcomputeres-PC>
2017-02-22 17:16:02 +0530

    by Bhavna

--- digital/src/main/java/com/visog/digital/model/Users.java
+++ digital/src/main/java/com/visog/digital/model/Users.java
@@ -6,4 +6,43 @@ package com.visog.digital.model;
 
 public class Users {
 
+	private String id;
+	private String name;
+	private String email;
+	private String password;
+	private String phone_no;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone_no() {
+		return phone_no;
+	}
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+	
+		
 }
